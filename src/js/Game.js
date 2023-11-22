d801c89f06d1805dd57408ddff9841d5e96fe8f3
Jacob DeBenedetto <deben3@gmail.com>
2023-11-21 17:08:12 -0700

    Add basic loop logic

--- src/js/Game.js
+++ src/js/Game.js
@@ -1,13 +1,15 @@
 import { OrthographicCamera, Scene, Vector3 } from 'three';
 import { ColliderDesc, RigidBodyDesc, World } from '@dimforge/rapier3d';
+import { Loop } from './Loop';
 
 class Game {
   constructor() {
-
+    this.loop = new Loop();
   }
 
   init() {
-    import('@dimforge/rapier3d').then(function(RAPIER) {
+    var _this = this;
+    import('@dimforge/rapier3d').then(function() {
       // Use the RAPIER module here.
       let gravity = { x: 0.0, y: -9.81, z: 0.0 };
       let world = new World(gravity);
@@ -23,20 +25,22 @@ class Game {
       // Create a cuboid collider attached to the dynamic rigidBody.
       let colliderDesc = ColliderDesc.cuboid(0.5, 0.5, 0.5);
       let collider = world.createCollider(colliderDesc, rigidBody);
-    
-      // Game loop. Replace by your own game loop system.
-      let gameLoop = () => {
-        // Ste the simulation forward.  
+
+      // Add physics loop
+      _this.loop.add(30, function(data) {
         world.step();
-    
-        // Get and print the rigid-body's position.
         let position = rigidBody.translation();
-        console.log("Rigid-body position: ", position.x, position.y, position.z);
-    
-        setTimeout(gameLoop, 16);
-      };
-    
-      gameLoop();
+        //console.log("Rigid-body position: ", position.x, position.y, position.z);
+        console.log('physic loop', data);
+      });
+
+      // Add render loop
+      _this.loop.add(-1, function(data) {
+        console.log('render loop', data);
+      });
+
+      // Start loop
+      _this.loop.start();
     });
   }
 }
--- src/js/Loop.js
+++ src/js/Loop.js
@@ -1,10 +1,82 @@
-import { Clock, PCFSoftShadowMap, WebGLRenderer } from 'three';
-import Stats from './Stats.js';
+// Add single or multiple synchronous loop intervals
 
 class Loop {
     constructor() {
-        this.stats = new Stats();
+        this.autoStart = true;
+        this.startTime = 0;
+        this.oldTime = 0;
+        this.elapsedTime = 0;
+        this.scale = 1;
+        this.running = false;
+        this.intervals = [];
     }
+
+    add(fps = 60, callback = function(){}) {
+        this.intervals.push({ tick: 1 / fps, sum: 0, alpha: 0, callback: callback });
+    }
+
+    start() {
+        this.startTime = this.now();
+        this.oldTime = this.startTime;
+        this.elapsedTime = 0;
+        this.running = true;
+        this.update(this);
+    }
+
+    stop() {
+        this.getElapsedTime();
+        this.running = false;
+        this.autoStart = false;
+    }
+
+    update(loop) {
+        if (this.running == true) {
+            // Request visual update function before next repaint
+            requestAnimationFrame(function(){ loop.update(loop); });
+    
+            // Loop through array of loops
+            if (this.intervals.length > 1) {
+                var delta = this.getDelta();
+                var alpha = this.intervals[0].sum / this.intervals[0].tick; // Set alpha to first interval
+
+                // Loop through loops (descending order)
+                for (var i = this.intervals.length - 1; i >= 0; i--) {
+                    var child = this.intervals[i];
+                    child.sum += delta;
+        
+                    // Trigger loop callback
+                    if (child.sum > child.tick || child.tick == -1) {
+                        child.sum %= child.tick;
+                        child.callback({ delta: delta, alpha: alpha });
+                    }
+                }
+            }
+        }
+    }
+
+    getElapsedTime() {
+        this.getDelta();
+        return this.elapsedTime;
+    }
+
+    getDelta() {
+        let diff = 0;
+        if (this.autoStart && !this.running) {
+            this.start();
+            return 0;
+        }
+        if (this.running) {
+            const newTime = this.now();
+            diff = (newTime - this.oldTime) / 1000;
+            this.oldTime = newTime;
+            this.elapsedTime += diff;
+        }
+        return diff * this.scale;
+    }
+
+    now() {
+		return ( typeof performance === 'undefined' ? Date : performance ).now(); // see #10732
+	}
 }
 
 export { Loop };
