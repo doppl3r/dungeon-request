842d70c37722e8910f3c3482c609ebc718b799f4
Jacob DeBenedetto <deben3@gmail.com>
2026-06-30 17:21:20 -0700

    Update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,13 +7,10 @@ export default defineConfig({
     hmr: false, // Disable hot reload on save
   },
   plugins: [
-    topLevelAwait(),
-    vue(),
-    wasm()
+    vue()
   ],
   base: './',
-    build: {
-      emptyOutDir: true
-    }
+  build: {
+    emptyOutDir: true
   }
-);
+});
